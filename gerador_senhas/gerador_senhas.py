51ffccd498ae0d5d64ddb25a84aec45df2ca397c
Antonio Neto <antoniogfneto92@hotmail.com>
2021-11-26 18:57:52 -0300

    update

--- gerador_senhas/gerador_senhas.py
+++ gerador_senhas/gerador_senhas.py
@@ -2,15 +2,32 @@ from random import SystemRandom
 from string import ascii_letters, digits
 
 
+class Error(Exception):
+    pass
+
+
+class SizeError(Error):
+    def __init__(self, message):
+        self.message = message
+
+
 def main():
     chars = ascii_letters + digits + 'çÇ!@#$%&*()-_++§"´`[]{}ªº~^,.<>;:/?°\\|' #  Registrando os caracteres desejados para a senha
     rnd = SystemRandom()
 
+    print('-'*60)
+    print('Gerador de Senha Forte Aleatório by Neto')
+    print('-'*60)
+
     while True:
         try:  #  Validando se a entrada é um número
-            tamanho = int(input('Digite a quantidade de dígitos de sua senha: '))
+            tamanho = int(input('Digite a quantidade de dígitos de sua senha (Mínimo de 8 caracteres): '))
+            if tamanho < 8:
+                raise SizeError('Tamanho menor que 8 caracteres')
         except ValueError as erro:
             print(f'Por favor, digite um número. Erro: {erro}')
+        except SizeError as erro:
+            print(erro)
         else:
             print('Senha gerada: ', end='')
             print(''.join(rnd.choice(chars) for i in range(tamanho))) #  Geração da senha aleatória com os caracteres registrados
