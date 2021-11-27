4593227e3ce07bb8ee8c12569d33c9486cd2bbfb
Antonio Neto <88252054+Neto002@users.noreply.github.com>
2021-11-27 18:31:49 -0300

    update
    adicionada a opção de salvar a senha

--- gerador_senhas/gerador_senhas.py
+++ gerador_senhas/gerador_senhas.py
@@ -1,28 +1,36 @@
 from random import SystemRandom
 from string import ascii_letters, digits
+from os.path import join
+import hashlib
 
 
 class Error(Exception):
     pass
 
 
-class SizeError(Error):
+class SizeError(Error):  # Criação de Erro para o tamanho da senha
     def __init__(self, message):
         self.message = message
 
 
+def salva_senha(senha):  # Método de gravação de hash da senha em diretório escolhido pelo usuário
+    diretorio = input('Digite o diretório desejado para o arquivo: ').strip()
+    arquivo = open(join(diretorio, 'senhas.txt'), 'a')
+    arquivo.write(f'{hashlib.sha1(senha.encode()).hexdigest()}\n') # Criptografando a senha em hash com algoritmo sha1
+
+
 def main():
-    chars = ascii_letters + digits + 'çÇ!@#$%&*()-_++§"´`[]{}ªº~^,.<>;:/?°\\|' #  Registrando os caracteres desejados para a senha
+    chars = ascii_letters + digits + 'çÇ!@#$%&*()-_++§"´`[]{}ªº~^,.<>;:/?°\\|'  # Registrando os caracteres desejados para a senha
     rnd = SystemRandom()
 
-    print('-'*60)
-    print('Gerador de Senha Forte Aleatório by Neto')
-    print('-'*60)
+    print('-' * 60)
+    print('Gerador de Senha Forte Aleatória by Neto')
+    print('-' * 60)
 
     while True:
-        try:  #  Validando se a entrada é um número
+        try:  # Validando se a entrada é um número
             tamanho = int(input('Digite a quantidade de dígitos de sua senha (Mínimo de 8 caracteres): '))
-            if tamanho < 8:  #  Validando se o tamanho é maior que 8 caracteres
+            if tamanho < 8:  # Validando se o tamanho é maior que 8 caracteres
                 raise SizeError('Tamanho menor que 8 caracteres')
         except ValueError as erro:
             print(f'Por favor, digite um número. Erro: {erro}')
@@ -30,10 +38,17 @@ def main():
             print(erro)
         else:
             print('Senha gerada: ', end='')
-            print(''.join(rnd.choice(chars) for i in range(tamanho))) #  Geração da senha aleatória com os caracteres registrados
-            resposta = input('Quer gerar mais uma senha? [S/N]').strip().upper()[0]
+            senha = ''.join(
+                rnd.choice(chars) for i in range(tamanho))  # Geração da senha aleatória com os caracteres registrados
+            print(senha)
+            resposta_gravacao = input('Deseja gravar essa senha em um arquivo? [S/N] ').strip().upper()[0]
+            while resposta_gravacao not in 'SN':
+                resposta_gravacao = input('Deseja gravar essa senha em um arquivo? [S/N] ').strip().upper()[0]
+            if resposta_gravacao == 'S':  # Gravação do hash da senha em arquivo
+                salva_senha(senha)
+            resposta = input('Quer gerar mais uma senha? [S/N] ').strip().upper()[0]
             while resposta not in 'SN':
-                resposta = input('Quer gerar mais uma senha? [S/N]').strip().upper()[0]
+                resposta = input('Quer gerar mais uma senha? [S/N] ').strip().upper()[0]
             if resposta == 'N':
                 print('Encerrando...')
                 break
